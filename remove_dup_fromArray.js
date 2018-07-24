b92e77fdc5da83d434a9ac7d7b44333796e12a8a
varshalodha <36828388+varshalodha@users.noreply.github.com>
2018-07-24 14:45:46 -0700

    Update remove_dup_fromArray.js

--- remove_dup_fromArray.js
+++ remove_dup_fromArray.js
@@ -1,31 +1,33 @@
-// function removeDuplicate(arr){
-//   var map = {};
-//   var resArr = [];
-//   for (let i=0; i< arr.length; i++){
-//     if(!map[arr[i]]){
-//       map[arr[i]] = true; // {1:true, 2:true}
-//       resArr.push(arr[i]);
-//     }
-//   }
-//   console.log(resArr);
-// }
+function removeDuplicate1(arr){
+  var map = {};
+  var resArr = [];
+  for (let i=0; i< arr.length; i++){
+    if(!map[arr[i]]){
+      map[arr[i]] = true; // {1:true, 2:true}
+      resArr.push(arr[i]);
+    }
+  }
+  console.log(resArr);
+}
 
-function removeDuplicate(arr) {
+function removeDuplicate2(arr) {
 	var set = new Set(arr);
 	console.log(Array.from(set));
 }
 
-// function removeDuplicate(arr){
-//   var res = arr.reduce((acc,val) => {
-//     if(acc.indexOf(val) == -1){
-//       acc.push(val);
-//     }
-//     return acc;
-//   },[]);
+function removeDuplicate3(arr){
+  var res = arr.reduce((acc,val) => {
+    if(acc.indexOf(val) == -1){
+      acc.push(val);
+    }
+    return acc;
+  },[]);
 
-//   console.log(res);
-// }
+  console.log(res);
+}
 
 var arr = [1, 2, 2, 4, 5, 3, 5, 18];
 
-removeDuplicate(arr);
+removeDuplicate1(arr);
+removeDuplicate2(arr);
+removeDuplicate3(arr);
