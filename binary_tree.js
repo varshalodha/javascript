f65b2d1cc0a6b5953fa522a1fc7cbb0d680249dd
varshalodha <36828388+varshalodha@users.noreply.github.com>
2018-07-25 16:30:33 -0700

    Update binary_tree.js

--- binary_tree.js
+++ binary_tree.js
@@ -1,61 +1,100 @@
-let BinarySearchTree = function() {
+var binaryTree = function() {
 	this.root = null;
 };
 
-let treeNode = function(value) {
+var treeNode = function(value) {
 	this.value = value;
 	this.left = null;
 	this.right = null;
 };
 
-BinarySearchTree.prototype.insert = function(value) {
-	let node = new treeNode(value);
-	let current = this.root;
+binaryTree.prototype.insert = function(value) {
+	var node = new treeNode(value);
+	var current = this.root;
+
 	if (!this.root) {
 		this.root = node;
 		return;
 	}
+
 	while (current) {
 		if (value < current.value) {
-			if (current.left === null) {
+			if (current.left == null) {
 				current.left = node;
 				break;
-			} else {
-				current = current.left;
 			}
+			current = current.left;
 		} else if (value > current.value) {
-			if (current.right === null) {
+			if (current.right == null) {
 				current.right = node;
 				break;
-			} else {
-				current = current.right;
 			}
-		} else {
-			break;
+			current = current.right;
 		}
 	}
 };
 
-BinarySearchTree.prototype.traverse = function() {
-	function inorder(node) {
-		if (node) {
-			if (node.left !== null) {
-				inorder(node.left);
-			}
+binaryTree.prototype.minValue = function() {
+	var current = this.root;
 
-			console.log(node.value);
-
-			if (node.right !== null) {
-				inorder(node.right);
-			}
-		}
+	if (!this.root) {
+		return null;
 	}
 
-	inorder(this.root);
+	if (!this.root.left) {
+		return root.value;
+	}
+
+	var min = current.value;
+	while (current) {
+		if (min > current.value) {
+			min = current.value;
+		}
+
+		if (current.left == null) {
+			return min;
+		}
+
+		current = current.left;
+	}
 };
-let bst = new BinarySearchTree();
-bst.insert(20);
-bst.insert(18);
-bst.insert(16);
-bst.insert(29);
-console.log(bst.traverse());
+
+binaryTree.prototype.maxValue = function() {
+	var current = this.root;
+
+	if (!this.root) {
+		return null;
+	}
+
+	if (!this.root.right) {
+		return root.value;
+	}
+
+	var max = current.value;
+	while (current) {
+		if (max < current.value) {
+			max = current.value;
+		}
+
+		if (current.right == null) {
+			return max;
+		}
+		current = current.right;
+	}
+};
+var print = function(node) {
+	if (node) {
+		print(node.left);
+		print(node.right);
+		console.log(node.value);
+	}
+};
+var bt = new binaryTree();
+bt.insert(40);
+bt.insert(25);
+bt.insert(25);
+bt.insert(10);
+bt.insert(32);
+bt.insert(78);
+
+
